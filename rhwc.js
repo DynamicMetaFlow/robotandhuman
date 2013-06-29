f13dc6deaf7d199600a10096f10b15c13f0bfe08
Triskaideka <octopus@triskaideka.net>
2013-06-29 14:41:11 -0400

    There is a long way still to go, but this is definitely taking shape.

--- rhwc.js
+++ rhwc.js
@@ -5,19 +5,65 @@ $(document).ready(function () {
   ajaxobj.onreadystatechange = function () {
     if (ajaxobj.readyState == 4)  {
       if (ajaxobj.status == 200)  {
-        var c = ajaxobj.responseText;
-        display_comics(c);
-      }  else  {
-        $('<p>Looks like your ol\' webmaster screwed up.  There was an error loading the comics.').insertAfter('#title');
+        display_comics( JSON.parse(ajaxobj.responseText) );
+      }  else  {  // This code never runs; figure out why
+        $("<p>Looks like your ol' webmaster screwed up.  There was an error loading the comics.</p>").insertAfter('#ctitle');
       }
     }
   }
 
-  // Construct the URL and then call it
-  ajaxobj.open("GET", "xcomics.json");
+  ajaxobj.open('GET', 'comics.json');
   ajaxobj.send(null);
 });
 
 function display_comics(comics)  {
+  var i = 0,   // loop watch variable for comics
+      j = 0,   // loop watch variable for panels
+      c = '';  // string containing all of the code for this comic
+
+  for (i = 0; i < comics.length; i++)  {
+    // We set the width of each episode div individually, according to how many panels it has.  If these numbers 
+    // seem mysterious, see .panel in rhwc.css for (at least partial) enlightenment.
+    c = '<div class="episode" style="width: '+(266*comics[i].panels.length)+'px">';
+
+    // title & date
+    c += '<div class="title">'+comics[i].title+'</div><div class="date">'+comics[i].date+'</div><br>';
+
+    // comic panels
+    for (j = 0; j < comics[i].panels.length; j++)  {
+      // the panel tag and its classes
+      c += '<div class="panel '+(comics[i].panels[j].extra_classes ? comics[i].panels[j].extra_classes : '')+'">';
+
+      // left character & dialogue
+      if (comics[i].panels[j].left.text)  {
+        c += '<div class="dialogue d-left">'+comics[i].panels[j].left.text+'</div>';
+      }
+      if (comics[i].panels[j].left.char)  {
+        c += '<img class="left" src="graphics/'+comics[i].panels[j].left.char+'.png">';
+      }
+
+      // right character & dialogue
+      if (comics[i].panels[j].right.text)  {
+        c += '<div class="dialogue d-right">'+comics[i].panels[j].right.text+'</div>';
+      }
+      if (comics[i].panels[j].right.char)  {
+        c += '<img class="right" src="graphics/'+comics[i].panels[j].right.char+'.png">';
+      }
+
+      c += '</div>\n';
+    }
+
+    // "show JSON" feature
+    c += '<div class="show_json"><a href="javascript:void(0)" onClick="$(this.parentNode).next().toggle()">&#x2b10; show/hide JSON</a></div>';
+    c += '<div class="json">'+JSON.stringify(comics[i], null, "  ")+'</div>';
+
+    // news
+    if (comics[i].news)  {
+      c += '<div class="news">'+comics[i].news+'</div>';
+    }
+
+    c += '</div>';  // class="episode"
+    $('<p>'+c+'</p>').insertBefore('#place');
+  }
 
 }
